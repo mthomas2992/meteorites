c65b34b6435dcc4a167f37c6a22ab57ccbac4d8f
Matthew Thomas <matt@afternight.io>
2017-03-23 13:43:30 +1100

    added names

--- Meteor/imports/app.jsx
+++ Meteor/imports/app.jsx
@@ -17,7 +17,12 @@ class App extends React.Component {
 
     render() {
       if (this.props.path == "Home"){
-        return (<h1>Home page for the meteorites</h1>);
+        return (<div><h1>Home page for the meteorites</h1>
+                <h2> The team </h2>
+                <p> Matthew Thomas </p>
+                <p> Mark Nerwich </p>
+                <p> Benjamin Phipps</p>
+                <p> Tomas Donovic</p></div>);
       } else {
         return (<p>404</p>);
       }
