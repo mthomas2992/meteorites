0106dee2c1bbfcb3681a7a5ed6d2d7b9aeb585a3
Matthew Thomas <matt@afternight.io>
2017-03-27 15:17:05 +1100

    added basic test

--- Meteor/imports/app.jsx
+++ Meteor/imports/app.jsx
@@ -9,20 +9,32 @@ class App extends React.Component {
       super(props);
 
       this.state = {
-
+        currentData:null
       };
 
     };
 
+    componentDidMount(){
+      var self = this;
+      Meteor.call('testAPIAccess',function(err,res){
+        console.log(res);
+        self.setState({currentData:res});
+      });
+    };
+
 
     render() {
+
       if (this.props.path == "Home"){
         return (<div><h1>Home page for the meteorites</h1>
                 <h2> The team </h2>
                 <p> Matthew Thomas </p>
                 <p> Mark Nerwich </p>
                 <p> Benjamin Phipps</p>
-                <p> Tomas Donovic</p></div>);
+                <p> Tomas Donovic</p>
+                <h2>Data test</h2>
+                <p>{this.state.currentData}</p>
+                </div>);
       } else {
         return (<p>404</p>);
       }
--- Meteor/server/main.js
+++ Meteor/server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
 
 Meteor.startup(() => {
 
@@ -22,10 +23,8 @@ Meteor.startup(() => {
 ////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////      ENDAPI          ///////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-  Meteor.methods ({
-    //add server side methods to be called with Meteor.call here
-    // add API calls to ABS here
+//add server side methods to be called with Meteor.call here
+// add API calls to ABS here
 
 /// ***************** API calls to ABS *****************
 
@@ -48,27 +47,35 @@ http://stat.data.abs.gov.au/sdmx-json/data/RT/0.2+1.20+41+42+43+44+45+46.10+20+3
 
 Example API call:
 // returns a list of ALBUMS
-      'getNewReleases' : function(authToken){
-        const options = {
-          "params": {
-            "limit": "30",
-            "country": "AU"
-          },
-          "headers" : {
-            "Authorization": "Bearer " + authToken
-          }
-        }
-        console.log("Called getNewReleases");
-        var result = HTTP.get("https://api.spotify.com/v1/browse/new-releases", options);
-        // console.log("THIS IS YOUR RESULT:\n")
-        // console.log(result);
-        return result;
+  'getNewReleases' : function(authToken){
+    const options = {
+      "params": {
+        "limit": "30",
+        "country": "AU"
       },
+      "headers" : {
+        "Authorization": "Bearer " + authToken
+      }
+    }
+    console.log("Called getNewReleases");
+    var result = HTTP.get("https://api.spotify.com/v1/browse/new-releases", options);
+    // console.log("THIS IS YOUR RESULT:\n")
+    // console.log(result);
+    return result;
+  },
 
 */
+
+  Meteor.methods ({
+
+    'testAPIAccess' :function (){
+      var queryString = "http://stat.data.abs.gov.au/sdmx-json/data/RT/0.2+1.20+41+42+43+44+45+46.10+20+30.M/all?startTime=2016-01&endTime=2017-01"
+      var result = HTTP.get(queryString);
+      var newresult = JSON.parse(result.content);
+      console.log(newresult);
+      return newresult;
+    }
 
 
   })
 });
-
-
