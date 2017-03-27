7bd2497c1995403578bfe2664b9054e9b28597e5
Matthew Thomas <matt@afternight.io>
2017-03-27 20:28:58 +1100

    added basic error checking for api requests

--- Meteor/server/main.js
+++ Meteor/server/main.js
@@ -13,6 +13,33 @@ Meteor.startup(() => {
     prettyJson: true
   });
 
+  function checkRequirements(requirements,queryParams){
+    var errorStack = new Array();
+    var warningStack = new Array();
+    for (i=0;i<requirements.length;i++){ //for each requirment of the given query
+      var currReq = requirements[i]; //set current
+      if (!queryParams[currReq.name] && currReq.required){ //if no queryParamater is detected that matches and it is a required query
+        errorStack.push({code:101, errorDetails:"\""+currReq.name+"\" is a required variable for this request, can be "+currReq.possibles});
+      } else if (queryParams[currReq.name]){ //if a query paramater does exist
+        var regex = currReq.expected.exec(queryParams[currReq.name]); //execute regex
+        var unexpectedError = {code:100, errorDetails:"Unexpected data value for \""+currReq.name+ "\", recieved \""+queryParams[currReq.name]+"\"expected "+currReq.possibles}
+        if (regex == null){ //if there are no matches to the regex at all
+          console.log(currReq.expected.exec(queryParams[currReq.name]));
+          errorStack.push(unexpectedError);
+        } else {
+          if (regex[0]!=regex['input']){ //if the regex matches multiples as opposed to one single match, useful for list checking
+            errorStack.push(unexpectedError);
+          }
+        }
+      }
+    }
+    if (requirements.length<queryParams.length){
+      errorStack.push({code:01, warningDetails:"Unnessasary paramaters detected, some given paramaters are not needed"});
+    }
+    var response = {requirements:requirements,errors:errorStack,warnings:warningStack};
+    return response;
+  };
+
   //add routes to the API here
   Api.addRoute('test', {authRequired: false}, {
     get: function () {
@@ -22,9 +49,24 @@ Meteor.startup(() => {
 
   Api.addRoute('retailTrade', {authRequired: false}, {
     get: function () {
+      //var expectedAreas = ["Retail","MerchandiseExports"];
+      var requirements = [{name:"statisticsArea", expected: new RegExp('(Retail|MerchandiseExports)','gi'),possibles:"one or multiple of \"Retail,MerchandiseExports\"",required:true},
+                          {name:"state", expected: new RegExp('((AUS|NSW|WA|SA|ACT|VIC|TAS|QLD|NT)(,+|$))+','gi'),possibles:"one or multiple of \"AUS,NSW,WA,SA,ACT,VIC,TAS,QLD,NT\"",required:true},
+                          {name:"category", expected: new RegExp('(.*)'), possibles:"stubbed", required:true},
+                          {name:"startDate", expected: new RegExp('(.*)'), possibles:"stubbed", required:true},
+                          {name:"endDate",expected: new RegExp('(.*)'), possibles:"stubbed", required:true}];
 
-
-      return {statusCode:202, body:{status:'Success', message:'Test success'}};
+      var errorsAndWarnings = checkRequirements(requirements,this.queryParams);
+      if (errorsAndWarnings.errors.length!=0){
+        return {statusCode:400, body:{status:errorsAndWarnings, data:'Errors prevented data request, check the status field'}}
+      } else {
+        //execute safe logic
+        var state = this.queryParams.state;
+        var category = this.queryParams.category;
+        var startDate = this.queryParams.startDate;
+        var endDate = this.queryParams.endDate;
+        return {statusCode:202, body:{status:'Success', message:'Test success'}};
+      }
     }
   });
 
@@ -82,11 +124,11 @@ Example API call:
       var newresult = JSON.parse(result.content);
       console.log(newresult);
       return newresult;
-    }
+    },
 
     'getRetailTurnover' : function(){
       //code for retail turnover here
-    }
+    },
 
     'getMerchandiseExports' : function(){
       //code for Merchandise exports turnover here
