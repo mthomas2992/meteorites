2fe640dbaa39cfba7a65b6b2da8d74469a87bab6
Matthew Thomas <matt@afternight.io>
2017-03-30 17:28:54 +1100

    documentation finishing touches

--- Meteor/imports/app/app.jsx
+++ Meteor/imports/app/app.jsx
@@ -17,20 +17,27 @@ class App extends React.Component {
     render() {
 
       if (this.props.path == "Home"){
-        return (<div id="home"><h1 id="header">Home page for the meteorites</h1>
-                <h2> What are we even doing here????</h2>
-                <p> After a rather successful attempt at SENG2021, we decided we had to try it <a>href="https://www.youtube.com/watch?v=FGBhQbmPwH8">one more time</a>. As a result, we are here, writing more really well written and honestly, better than ever solutions to arbitrary problems that we are given. Given this is the last SENG workshop, we want to make it count and do even better than last time. Thats why we bought a URL this time ( ͡° ͜ʖ ͡°)</p>
-                <br></br>
-                <h2> But who are we?</h2>
-                <p> Matthew Thomas </p>
-                <p> Mark Nerwich </p>
-                <p> Benjamin Phipps</p>
-                <p> <a href="https://www.instagram.com/tdonovic/">Tomas Donovic</a></p>
-                <h2> But you say, this stuff isn't even important, give us the project!!</h2>
-                <p> To this we would reply, you are probably right, you can find our <a href="http://meteoristics.com/api/documentation?ver=v1">API docs here</a>. We could point you to how to make an API call from this page, but you are honestly better off just checking out the extremely well written and all encompassing support docs. If you get stuck, be sure to tell us <a href="mailto:tomasdonovic@gmail.com">here</a></p>
+        return (<div id="home" className="container-fluid">
+                  <div id="homeHeader" className="row">
+                    <div id="header" className="col-md-12">
+                      Meteoristics
+                    </div>
+                    <div id="docoSpacer" className="col-md-12"></div>
+                  </div>
+                  <div id = "homeInfo"> <a href="http://meteoristics.com/api/documentation?ver=v1"><h1> Meteoristics API Documentation </h1></a>
+                      <br></br>
+
+                  <h2> What is Meteoristics?</h2>
+                  <p>Meteoristics is currently purely an API that allows for easy access to the Australian Bureau of Statistics data</p>
+                  <p> In the future this will be our home page for our graphical interface, but for now we do not have anything to put here</p>
+                  <h2> Our Team (Meteorites)</h2>
+                  <p> Matthew Thomas </p>
+                  <p> Mark Nerwich </p>
+                  <p> Benjamin Phipps</p>
+                  <p> <a href="https://www.instagram.com/tdonovic/">Tomas Donovic</a></p></div>
                 </div>);
       } else {
-        return (<p>404</p>);
+        return (<div><a href="http://meteoristics.com/api/documentation?ver=v1"><h1> Meteoristics API Documentation </h1></a> <p>404</p></div>);
       }
 
     }
