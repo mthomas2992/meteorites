e6a982ab4bc3d74d70b4f0189481e4a6824b6eac
Matthew Thomas <matt@afternight.io>
2017-04-13 12:31:46 +1000

    changed layout

--- Meteor/imports/app/briefs.jsx
+++ Meteor/imports/app/briefs.jsx
@@ -12,7 +12,8 @@ class Briefs extends React.Component {
         currentIndustry: this.props.status.industry,
         currentTimePeriodStart: this.props.status.timePeriodStart,
         currentTimePeriodEnd: this.props.status.timePeriodEnd,
-        currentStatesData: null
+        currentStatesData: null,
+        loading: true
       }
 
       this.loadData = this.loadData.bind(this);
@@ -27,15 +28,20 @@ class Briefs extends React.Component {
     };
 
     loadData(newProps){
+      console.log("called Loading" );
+      this.setState({loading:true});
+      console.log(newProps);
       this.setState({currentStates:newProps.status.selectedStates,currentIndustry:newProps.status.industry,currentTimePeriodStart:newProps.status.timePeriodStart,currentTimePeriodEnd:newProps.status.timePeriodEnd})
       var newCurrStateData = {};
       var self =this;
-      for (i=0;i<this.state.currentStates.length;i++){
-        Meteor.call('getAllTotalsOverTimeRetail',this.state.currentStates[i],this.state.currentTimePeriodStart,this.state.currentTimePeriodEnd, function (err,res){
+      for (i=0;i<newProps.status.selectedStates.length;i++){
+        Meteor.call('getAllTotalsOverTimeRetail',newProps.status.selectedStates[i],newProps.status.timePeriodStart,newProps.status.timePeriodEnd, function (err,res){
           newCurrStateData[res.state] = res.data;
           self.setState({currentStatesData:newCurrStateData});
+          self.setState({loading:false}); //only good when there is just one state
         });
       }
+
     }
 
     componentDidMount(){
@@ -43,11 +49,21 @@ class Briefs extends React.Component {
     };
 
     render() {
-      if (this.state.currentStates.length <=1){
-        console.log(this.state.currentStatesData);
-        return(<div> State data for {this.state.currentStates[0]}</div>);
+      if (this.state.loading){
+        return (<div>Loading....</div>);
       } else {
-        return (<div> Error </div>);
+        if (this.state.currentStates.length <=1){
+          //create table from given data
+          console.log(this.state.currentStatesData);
+          return(<div className= "col-md-12">
+                  <div id = "singularTitle" className="row"> State data for {this.state.currentStates[0]}</div>
+                  <div id= "singularGraphSection" className="row"> Graphs will go here </div>
+                  <div id= "singularTableData" className="row">Data tables will go here</div>
+                  
+                </div>);
+        } else {
+          return (<div> Error </div>);
+        }
       }
     }
 }
