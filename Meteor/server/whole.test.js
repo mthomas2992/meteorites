b94421d0f508974d83d0a74d13b39d810a0d3095
Tomas Donovic <tomasdonovic@gmail.com>
2017-04-25 12:32:06 +1000

    Tests now correctly running

--- Meteor/server/whole.test.js
+++ Meteor/server/whole.test.js
@@ -1,24 +1,50 @@
 import {Meteor} from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
 
-var chai = require('chai')
-  , chaiJsonEqual = require('chai-json-equal');
+var should = require('chai').should(),
+expect = require('chai').expect,
+supertest = require('supertest'),
 
-chai.use(chaiJsonEqual);
+api = supertest('http://localhost:3000');
+ 
+describe("Basic Tests", function(){
+	describe("Does it return a 200?", function(){
+		it("Returns a 200", function(done){
+			api.get('/api/v2/isApiAlive')
+			.set('Accept', 'application/json')
+			.expect(200, done);
+		});
+	});
 
+	/*describe("Does JSON returned have keys and values ", function(done){
+		api.get('/api/v2/')
+		.set('Accept', 'application/json')
+		.expect(200)
+		.end(function(err, res){
+			expect(res.body).to.have.property("")
+		}
+	}*/
+});
 
 describe("Initial Output Tester", function() {
 	describe("Test Case 1", function() {
 		it("Returns expected output", function(){
-			var tests = JSON.parse(Assets.getText("tests/t1.test"));
+			var tests = Assets.getText("tests/t1.test");
 			var test1 = "http://meteoristics.com/api/v2/RetailAndExports?statisticsArea=Retail&state=nsw&category=Food&startDate=2015-09-09&endDate=2016-09-09";		
-			var ret = JSON.parse(HTTP.get(test1));
-			chai.expect(tests).to.equal(ret);
+			api.get('api/v2/RetailAndExports?statisticsArea=Retail&state=nsw&category=Food&startDate=2015-09-09&endDate=2016-09-09')
+			.expect(200)
+			.end(function(err, res){
+				expect(res.body).to.eql(tests);
+				expect(res.body).to.not.equal(null);
+				done();
+			});
 		});
 	});
 });
+
+
 describe("test case 2", function(){
 	it("does the thing, surely", function(){
-		chai.expect('1').to.equal('1');
+		expect('1').to.equal('1');
 	});
 });	
