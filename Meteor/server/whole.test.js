646023e624e94bdbbbf438be2ccd1fa87b9d2e60
Matthew Thomas <matt@afternight.io>
2017-04-26 22:32:58 +1000

    added testing framework

--- Meteor/server/whole.test.js
+++ Meteor/server/whole.test.js
@@ -1,50 +1,43 @@
 import {Meteor} from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
 
-var should = require('chai').should(),
-expect = require('chai').expect,
-supertest = require('supertest');
-var testfile = "tests/bbtest.json";
+expect = require('chai').expect
 
-api = supertest('http://localhost:3000');
- 
-describe("Basic Tests", function(){
-	describe("Does isApiAlive return a 200?", function(){
-		it("Returns a 200", function(done){
-			api.get('/api/v2/isApiAlive')
-			.set('Accept', 'application/json')
-			.expect(200, done);
-		});
-	});
-	describe("Does RetailAndExports return a 200?", function(){
-		it("Returns a 200", function(done){
-			api.get('/api/v2/isApiAlive')
-			.set('Accept', 'application/json')
-			.expect(200, done);
-		});
-	});
-});
-
-describe("Black Box Output Tester", function() {
-	var testData = {};
-	testData = JSON.parse(Assets.getText(testfile));
-	var testNo = 1;
-	for (var key in testData){
-		if (testData.hasOwnProperty(key)){
-			it(testNo + " Black Box test has passed", function(){
-				api.get(key)
-				.expect(200)
-				.set('Accept', 'application/json')
-				.end(function(err,res){
-					Meteor.bindEnvironment(function(error,result){
-						expect(res.body).to.eql(JSON.parse(Assets.getText(testData['tests/responses/'+key])));
-					});
-					expect(res.body).to.not.equal(null);
-				});
-			});
-		}
-	}
-});
+const rootUrl = "http://meteoristics.com/api/";
 
 
+//function that will compare alpha and beta, ignoring the keys and their children of the array keystoignore
+function JSONComparator(fileAlpha,fileBeta,keysToIgnore){
+  // console.log(fileAlpha);
+  // console.log(fileBeta);
+  expect(true).to.equal(true);
+}
 
+var testIndex = JSON.parse(Assets.getText("tests/index.json"));
+
+//Api testing
+for (i=0;i<testIndex.versions.length;i++){ //for each version
+  var curr = testIndex.versions[i];
+
+  console.log("BlackBox Testing version "+curr.versionID);
+  for (j=0;j<curr.blackBoxTests.length;j++){ //for each of the endpoints
+    var currEndpointTesting = curr.blackBoxTests[j];
+    describe("Endpoint "+currEndpointTesting.endpoint, function(){
+      for (k=0;k<currEndpointTesting.tests.length;k++){ //for each of the endpoints tests
+        var testFile = JSON.parse(Assets.getText("tests/"+curr.versionID+"/blackbox/"+currEndpointTesting.endpoint+"/"+currEndpointTesting.tests[k]+".json"));
+        it (testFile.Name, function (){
+            var test=HTTP.get(rootUrl+curr.versionID+testFile.Query);
+            JSONComparator(testFile.Expected,test);
+        })
+      }
+    })
+  }
+
+}
+
+// describe("Black Box Tests", function() {
+//   console.log(testIndex);
+//   it("Black Box test has passed", function(){
+//
+//   });
+// });
--- Meteor/tests/test.js
+++
@@ -1,8 +0,0 @@
-import {Meteor} from 'meteor/meteor';
-import { HTTP } from 'meteor/http';
-
-var expect = require("chai").expect;
-var jsonEqual = require('chai-json-equal');
-describe('Initial Output Tester', function() {
-});
-
