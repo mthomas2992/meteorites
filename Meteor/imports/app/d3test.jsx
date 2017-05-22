36b9081b8fa305ced83b9deb24f895ad350811c9
BenjaminPhipps <benjaminphipps13@gmail.com>
2017-05-22 17:50:10 +1000

    graph appearing in the wrong place on the impact screen

--- Meteor/imports/app/d3test.jsx
+++ Meteor/imports/app/d3test.jsx
@@ -319,106 +319,45 @@ class D3Test extends React.Component {
            ]
           }
          ]
-        },
+        }
       }
+      this.doEverything = this.doEverything.bind(this);
+      this.stash = this.stash.bind(this);
+      this.arcTween = this.arcTween(this);
     }
 
   componentWillMount(){
-    var width = 960,
-    height = 700,
-    radius = Math.min(width, height) / 2,
-    color = d3.scale.category20c();
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height * .52 + ")");
-
-var partition = d3.layout.partition()
-    .sort(null)
-    .size([2 * Math.PI, radius * radius])
-    .value(function(d) { return 1; });
-
-var arc = d3.svg.arc()
-    .startAngle(function(d) { return d.x; })
-    .endAngle(function(d) { return d.x + d.dx; })
-    .innerRadius(function(d) { return Math.sqrt(d.y); })
-    .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
-
-  var root = this.state.rooty;
-  var path = svg.datum(root).selectAll("path")
-      .data(partition.nodes)
-    .enter().append("path")
-      .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
-      .attr("d", arc)
-      .style("stroke", "#fff")
-      .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
-      .style("fill-rule", "evenodd")
-      .each(stash);
-
-  d3.selectAll("input").on("change", function change() {
-    var value = this.value === "count"
-        ? function() { return 1; }
-        : function(d) { return d.size; };
-
-    path
-        .data(partition.value(value).nodes)
-      .transition()
-        .duration(1500)
-        .attrTween("d", arcTween);
-  });
-
-// Stash the old values for transition.
-function stash(d) {
-  d.x0 = d.x;
-  d.dx0 = d.dx;
-}
-
-// Interpolate the arcs in data space.
-function arcTween(a) {
-  var i = d3.interpolate({x: a.x0, dx: a.dx0}, a);
-  return function(t) {
-    var b = i(t);
-    a.x0 = b.x;
-    a.dx0 = b.dx;
-    return arc(b);
-  };
-}
-
-d3.select(self.frameElement).style("height", height + "px");
-
-
-  var path = svg.datum(root).selectAll("path")
-      .data(partition.nodes)
-    .enter().append("path")
-      .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
-      .attr("d", arc)
-      .style("stroke", "#fff")
-      .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
-      .style("fill-rule", "evenodd")
-      .each(stash);
-
-    d3.selectAll("input").on("change", function change() {
-      var value = this.value === "count"
-          ? function() { return 1; }
-          : function(d) { return d.size; };
-
-      path
-          .data(partition.value(value).nodes)
-        .transition()
-          .duration(1500)
-          .attrTween("d", arcTween);
-    });
+    this.doEverything();
   }
 
   componentDidUpdate(){
-    var width = 960,
-    height = 700,
+    this.doEverything();
+  }
+
+    // Stash the old values for transition.
+stash(d) {
+  d.x0 = d.x;
+  d.dx0 = d.dx;
+}
+
+// Interpolate the arcs in data space.
+arcTween(a) {
+  var i = d3.interpolate({x: a.x0, dx: a.dx0}, a);
+  return function(t) {
+    var b = i(t);
+    a.x0 = b.x;
+    a.dx0 = b.dx;
+    return arc(b);
+  };
+}
+
+doEverything() {
+    var width = 125,
+    height = 100,
     radius = Math.min(width, height) / 2,
     color = d3.scale.category20c();
 
-var svg = d3.select("body").append("svg")
+  var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
   .append("g")
@@ -444,7 +383,7 @@ var arc = d3.svg.arc()
       .style("stroke", "#fff")
       .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
       .style("fill-rule", "evenodd")
-      .each(stash);
+      .each(this.stash);
 
   d3.selectAll("input").on("change", function change() {
     var value = this.value === "count"
@@ -455,25 +394,9 @@ var arc = d3.svg.arc()
         .data(partition.value(value).nodes)
       .transition()
         .duration(1500)
-        .attrTween("d", arcTween);
+        .attrTween("d", this.arcTween);
   });
 
-// Stash the old values for transition.
-function stash(d) {
-  d.x0 = d.x;
-  d.dx0 = d.dx;
-}
-
-// Interpolate the arcs in data space.
-function arcTween(a) {
-  var i = d3.interpolate({x: a.x0, dx: a.dx0}, a);
-  return function(t) {
-    var b = i(t);
-    a.x0 = b.x;
-    a.dx0 = b.dx;
-    return arc(b);
-  };
-}
 
 d3.select(self.frameElement).style("height", height + "px");
 
@@ -486,7 +409,7 @@ d3.select(self.frameElement).style("height", height + "px");
       .style("stroke", "#fff")
       .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
       .style("fill-rule", "evenodd")
-      .each(stash);
+      .each(this.stash);
 
   d3.selectAll("input").on("change", function change() {
     var value = this.value === "count"
@@ -497,24 +420,16 @@ d3.select(self.frameElement).style("height", height + "px");
         .data(partition.value(value).nodes)
       .transition()
         .duration(1500)
-        .attrTween("d", arcTween);
+        .attrTween("d", this.arcTween);
   });
   }
 
-
 render() {
-
       return(
         <div>
-        <form>
-          <label><input type="radio" name="mode" defaultValue="size" /> Size</label>
-          <label><input type="radio" name="mode" defaultValue="count" defaultChecked /> Count</label>
-        </form>
         </div>
         );
     }
 }
-
-
 
 export default D3Test;
