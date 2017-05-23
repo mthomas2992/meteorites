215f89929dff9b91c7daec861d12919f1ff9922c
Matthew Thomas <matt@afternight.io>
2017-05-23 13:38:24 +1000

    added basic home page

--- Meteor/imports/app/d3test.jsx
+++ Meteor/imports/app/d3test.jsx
@@ -337,144 +337,144 @@ class D3Test extends React.Component {
   }
 
     // Stash the old values for transition.
-stash(d) {
-  d.x0 = d.x;
-  d.dx0 = d.dx;
-}
-
-// Interpolate the arcs in data space.
-arcTween(a) {
-  var i = d3.interpolate({x: a.x0, dx: a.dx0}, a);
-  return function(t) {
-    var b = i(t);
-    a.x0 = b.x;
-    a.dx0 = b.dx;
-    return arc(b);
-  };
-}
-
-doEverything() {
-    var width = 480,
-    height = 350,
-    radius = Math.min(width, height) / 2,
-    color = d3.scale.category20c();
-
-  var svg = d3.select(".d3Holder").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height * .52 + ")");
-
-var partition = d3.layout.partition()
-    .sort(null)
-    .size([2 * Math.PI, radius * radius])
-    .value(function(d) { return 1; });
-
-var arc = d3.svg.arc()
-    .startAngle(function(d) { return d.x; })
-    .endAngle(function(d) { return d.x + d.dx; })
-    .innerRadius(function(d) { return Math.sqrt(d.y); })
-    .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
-
-  var root = this.state.rooty;
-
-  var path = svg.datum(root).selectAll("path")
-      .data(partition.nodes)
-    .enter().append("path")
-      .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
-      .attr("d", arc)
-      .style("stroke", "#fff")
-      .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
-      .style("fill-rule", "evenodd")
-      .each(this.stash)
-      .on("mouseover", this.mouseover)
-      .on("mouseout", this.mouseleave);
-
-  d3.selectAll("input").on("change", function change() {
-    var value = this.value === "count"
-        ? function() { return 1; }
-        : function(d) { return d.size; };
-
-    path
-        .data(partition.value(value).nodes)
-      .transition()
-        .duration(1500)
-        .attrTween("d", this.arcTween);
-  });
-
-
-d3.select(self.frameElement).style("height", height + "px");
-
-
+  stash(d) {
+    d.x0 = d.x;
+    d.dx0 = d.dx;
   }
 
+  // Interpolate the arcs in data space.
+  arcTween(a) {
+    var i = d3.interpolate({x: a.x0, dx: a.dx0}, a);
+    return function(t) {
+      var b = i(t);
+      a.x0 = b.x;
+      a.dx0 = b.dx;
+      return arc(b);
+    };
+  }
+
+  doEverything() {
+      var width = 480,
+      height = 350,
+      radius = Math.min(width, height) / 2,
+      color = d3.scale.category20c();
+
+    var svg = d3.select(".d3Holder").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height * .52 + ")");
+
+  var partition = d3.layout.partition()
+      .sort(null)
+      .size([2 * Math.PI, radius * radius])
+      .value(function(d) { return 1; });
+
+  var arc = d3.svg.arc()
+      .startAngle(function(d) { return d.x; })
+      .endAngle(function(d) { return d.x + d.dx; })
+      .innerRadius(function(d) { return Math.sqrt(d.y); })
+      .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
+
+    var root = this.state.rooty;
+
+    var path = svg.datum(root).selectAll("path")
+        .data(partition.nodes)
+      .enter().append("path")
+        .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
+        .attr("d", arc)
+        .style("stroke", "#fff")
+        .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+        .style("fill-rule", "evenodd")
+        .each(this.stash)
+        .on("mouseover", this.mouseover)
+        .on("mouseout", this.mouseleave);
+
+    d3.selectAll("input").on("change", function change() {
+      var value = this.value === "count"
+          ? function() { return 1; }
+          : function(d) { return d.size; };
+
+      path
+          .data(partition.value(value).nodes)
+        .transition()
+          .duration(1500)
+          .attrTween("d", this.arcTween);
+    });
 
 
-  // Fade all but the current sequence, and show it in the breadcrumb trail.
-mouseover(d) {
-console.log("MOUSE OVER NOW");
+  d3.select(self.frameElement).style("height", height + "px");
 
-  // d3.select(this)
-  //       .transition()
-  //       .duration(1000)
-  //       .ease('elastic')
-  //       .style("opacity", 0.3);
 
-  // var percentage = (100 * d.value / totalSize).toPrecision(3);
-  // var percentageString = percentage + "%";
-  // if (percentage < 0.1) {
-  //   percentageString = "< 0.1%";
-  // }
-
-  // d3.select("#percentage")
-  //     .text(percentageString);
-
-  // d3.select("#explanation")
-  //     .style("visibility", "");
-
-  // var sequenceArray = getAncestors(d);
-  //updateBreadcrumbs(sequenceArray, percentageString);
-
-  // Fade all the segments.
-  // d3.selectAll("path")
-  //     .style("opacity", 0.3);
-
-  // Then highlight only those that are an ancestor of the current segment.
-  // vis.selectAll("path")
-  //     .filter(function(node) {
-  //               return (sequenceArray.indexOf(node) >= 0);
-  //             })
-  //     .style("opacity", 1);
-}
-
-// Restore everything to full opacity when moving off the visualization.
-mouseleave(d) {
-console.log("MOUSELEAVE NOW");
-  // Hide the breadcrumb trail
-  // d3.select("#trail")
-  //     .style("visibility", "hidden");
-
-  // // Deactivate all segments during transition.
-  // d3.selectAll("path").on("mouseover", null);
-
-  // // Transition each segment to full opacity and then reactivate it.
-  // d3.selectAll("path")
-  //     .transition()
-  //     .duration(1000)
-  //     .style("opacity", 1)
-  //     .on("end", function() {
-  //             d3.select(this).on("mouseover", mouseover);
-  //           });
-
-  // d3.select("#explanation")
-  //     .style("visibility", "hidden");
-}
-
-render() {
-      return(
-        <div className="d3Holder"></div>
-        );
     }
-}
 
-export default D3Test;
+
+
+    // Fade all but the current sequence, and show it in the breadcrumb trail.
+  mouseover(d) {
+  console.log("MOUSE OVER NOW");
+
+    // d3.select(this)
+    //       .transition()
+    //       .duration(1000)
+    //       .ease('elastic')
+    //       .style("opacity", 0.3);
+
+    // var percentage = (100 * d.value / totalSize).toPrecision(3);
+    // var percentageString = percentage + "%";
+    // if (percentage < 0.1) {
+    //   percentageString = "< 0.1%";
+    // }
+
+    // d3.select("#percentage")
+    //     .text(percentageString);
+
+    // d3.select("#explanation")
+    //     .style("visibility", "");
+
+    // var sequenceArray = getAncestors(d);
+    //updateBreadcrumbs(sequenceArray, percentageString);
+
+    // Fade all the segments.
+    // d3.selectAll("path")
+    //     .style("opacity", 0.3);
+
+    // Then highlight only those that are an ancestor of the current segment.
+    // vis.selectAll("path")
+    //     .filter(function(node) {
+    //               return (sequenceArray.indexOf(node) >= 0);
+    //             })
+    //     .style("opacity", 1);
+  }
+
+  // Restore everything to full opacity when moving off the visualization.
+  mouseleave(d) {
+  console.log("MOUSELEAVE NOW");
+    // Hide the breadcrumb trail
+    // d3.select("#trail")
+    //     .style("visibility", "hidden");
+
+    // // Deactivate all segments during transition.
+    // d3.selectAll("path").on("mouseover", null);
+
+    // // Transition each segment to full opacity and then reactivate it.
+    // d3.selectAll("path")
+    //     .transition()
+    //     .duration(1000)
+    //     .style("opacity", 1)
+    //     .on("end", function() {
+    //             d3.select(this).on("mouseover", mouseover);
+    //           });
+
+    // d3.select("#explanation")
+    //     .style("visibility", "hidden");
+  }
+
+  render() {
+        return(
+          <div className="d3Holder"></div>
+          );
+      }
+  }
+
+  export default D3Test;
--- Meteor/imports/app/mainHome.jsx
+++ Meteor/imports/app/mainHome.jsx
@@ -16,17 +16,14 @@ class MainHome extends React.Component {
 
     render() {
       return (<div className = "row">
-                <br></br>
-                <div onClick = {()=>{FlowRouter.go("/impact?title=Yasi&startDate=2011-01-09&endDate=2011-03-09")}}className = "col-md-10 col-md-offset-1" id = "yasiRoot">
-                  <div className = "row">
-                    <div id= "eventTitle" className = "col-md-6">
-                      <div className = "row" id = "title">Cyclone Yasi</div>
-                      <div id = "dates"> 2011-01 to 2011-03</div>
-                    </div>
-                    <div className= "col-md-6">
-                      <img src ="images/yasi.png" id = "yasiImage"></img>
-                    </div>
-                  </div>
+                <div onClick = {()=>{FlowRouter.go("/impact?title=Yasi&startDate=2011-01-09&endDate=2011-03-09")}}className = "col-md-7" id = "yasiRoot">
+                  Yasi clyclone
+                </div>
+                <div onClick = {()=>{FlowRouter.go('/impact?title=CycloneLarry&startDate=2009-01-09&endDate=2009-03-09')}} className = "col-md-5" id= "rightTop">
+                  Black Saturday bushfires
+                </div>
+                <div onClick = {()=>{FlowRouter.go('/impact?title=CycloneLarry&startDate=2006-02-09&endDate=2006-04-09')}} className = "col-md-5" id = "rightBottom">
+                  Cyclone larry
                 </div>
 
               </div>)
