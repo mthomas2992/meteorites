89bebd5e32c894945778ea1d74083f8bffe22d2e
BenjaminPhipps <benjaminphipps13@gmail.com>
2017-05-23 13:32:55 +1000

    check that mouseover rn

--- Meteor/imports/app/d3test.jsx
+++ Meteor/imports/app/d3test.jsx
@@ -323,9 +323,10 @@ class D3Test extends React.Component {
       }
       this.doEverything = this.doEverything.bind(this);
       this.stash = this.stash.bind(this);
-      this.arcTween = this.arcTween(this);
-      this.mouseover = this.mouseover(this);
-      this.mouseleave = this.mouseleave(this);
+      this.arcTween = this.arcTween.bind(this);
+      this.handleMouseOver = this.handleMouseOver.bind(this);
+      this.mouseleave = this.mouseleave.bind(this);
+      this.getAncestors = this.getAncestors.bind(this);
     }
 
   componentDidMount(){
@@ -336,84 +337,9 @@ class D3Test extends React.Component {
     this.doEverything();
   }
 
-    // Stash the old values for transition.
-stash(d) {
-  d.x0 = d.x;
-  d.dx0 = d.dx;
-}
-
-// Interpolate the arcs in data space.
-arcTween(a) {
-  var i = d3.interpolate({x: a.x0, dx: a.dx0}, a);
-  return function(t) {
-    var b = i(t);
-    a.x0 = b.x;
-    a.dx0 = b.dx;
-    return arc(b);
-  };
-}
-
-doEverything() {
-    var width = 480,
-    height = 350,
-    radius = Math.min(width, height) / 2,
-    color = d3.scale.category20c();
-
-  var svg = d3.select(".d3Holder").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height * .52 + ")");
-
-var partition = d3.layout.partition()
-    .sort(null)
-    .size([2 * Math.PI, radius * radius])
-    .value(function(d) { return 1; });
-
-var arc = d3.svg.arc()
-    .startAngle(function(d) { return d.x; })
-    .endAngle(function(d) { return d.x + d.dx; })
-    .innerRadius(function(d) { return Math.sqrt(d.y); })
-    .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
-
-  var root = this.state.rooty;
-
-  var path = svg.datum(root).selectAll("path")
-      .data(partition.nodes)
-    .enter().append("path")
-      .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
-      .attr("d", arc)
-      .style("stroke", "#fff")
-      .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
-      .style("fill-rule", "evenodd")
-      .each(this.stash)
-      .on("mouseover", this.mouseover)
-      .on("mouseout", this.mouseleave);
-
-  d3.selectAll("input").on("change", function change() {
-    var value = this.value === "count"
-        ? function() { return 1; }
-        : function(d) { return d.size; };
-
-    path
-        .data(partition.value(value).nodes)
-      .transition()
-        .duration(1500)
-        .attrTween("d", this.arcTween);
-  });
-
-
-d3.select(self.frameElement).style("height", height + "px");
-
-
-  }
-
-
-
   // Fade all but the current sequence, and show it in the breadcrumb trail.
-mouseover(d) {
+handleMouseOver(d) {
 console.log("MOUSE OVER NOW");
-
   // d3.select(this)
   //       .transition()
   //       .duration(1000)
@@ -469,6 +395,131 @@ console.log("MOUSELEAVE NOW");
   // d3.select("#explanation")
   //     .style("visibility", "hidden");
 }
+
+
+
+    // Stash the old values for transition.
+stash(d) {
+  d.x0 = d.x;
+  d.dx0 = d.dx;
+}
+
+// Interpolate the arcs in data space.
+arcTween(a) {
+  var i = d3.interpolate({x: a.x0, dx: a.dx0}, a);
+  return function(t) {
+    var b = i(t);
+    a.x0 = b.x;
+    a.dx0 = b.dx;
+    return arc(b);
+  };
+}
+
+getAncestors(node) {
+  var path = [];
+  var current = node;
+  while (current.parent) {
+    path.unshift(current);
+    current = current.parent;
+  }
+  return path;
+}
+
+doEverything() {
+    var width = 480,
+    height = 350,
+    radius = Math.min(width, height) / 2,
+    color = d3.scale.category20c();
+
+  var svg = d3.select(".d3Holder").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height * .52 + ")");
+
+var partition = d3.layout.partition()
+    .sort(null)
+    .size([2 * Math.PI, radius * radius])
+    .value(function(d) { return 1; });
+
+var arc = d3.svg.arc()
+    .startAngle(function(d) { return d.x; })
+    .endAngle(function(d) { return d.x + d.dx; })
+    .innerRadius(function(d) { return Math.sqrt(d.y); })
+    .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
+
+  var root = this.state.rooty;
+
+  svg.append("svg:circle")
+      .attr("r", radius)
+      .style("opacity", 0);
+
+  var path = svg.datum(root).selectAll("path")
+      .data(partition.nodes)
+    .enter().append("path")
+      .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
+      .attr("d", arc)
+      .style("stroke", "#fff")
+      .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+      .style("fill-rule", "evenodd")
+      .each(this.stash)
+      .on("mouseover", function(d){
+        //this.handleMouseOver(d);
+        console.log("MOUSING OVER");
+
+        var path = [];
+        var current = d;
+        while (current.parent) {
+        path.unshift(current);
+        current = current.parent;
+        }
+
+        //var sequenceArray = this.getAncestors(d);
+        var sequenceArray = path;
+
+        //fade everything
+        d3.selectAll("path")
+            .style("opacity", 0.3);
+
+        // Then highlight only those that are an ancestor of the current segment.
+        svg.selectAll("path")
+            .filter(function(node) {
+                      return (sequenceArray.indexOf(node) >= 0);
+                    })
+            .style("opacity", 1);
+      })
+      .on("mouseout", function(d){
+        console.log("MOUSE LEAVING");
+          // // Transition each segment to full opacity and then reactivate it.
+        d3.selectAll("path")
+            .transition()
+            .duration(1000)
+            .style("opacity", 1);
+            // .on("end", function() {
+            //         d3.select(this).on("mouseover", mouseover);
+            //       });
+      });
+
+  d3.selectAll("input").on("change", function change() {
+    var value = this.value === "count"
+        ? function() { return 1; }
+        : function(d) { return d.size; };
+
+    path
+        .data(partition.value(value).nodes)
+      .transition()
+        .duration(1500)
+        .attrTween("d", this.arcTween);
+  });
+
+
+d3.select(self.frameElement).style("height", height + "px");
+
+
+  }
+
+
+
 
 render() {
       return(
